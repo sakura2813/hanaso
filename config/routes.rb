c93480eb13c6f3b12b594db4f74676d295737dd0
sakura2813 <i.wo.u.813@gmail.com>
2025-08-05 23:39:14 +0900

    チャットとメッセージ用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
   root to: 'home#index'
+  resources :chats, only: [:index, :show, :new] do
+    resources :messages, only: [:create]
+  end
 end

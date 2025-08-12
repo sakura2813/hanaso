d20a5db4bdde46b309cb32c917a3b6fbdf26383c
sakura2813 <i.wo.u.813@gmail.com>
2025-08-12 12:46:35 +0900

    コントローラにCRUD追加

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -3,11 +3,41 @@ class SymptomsController < ApplicationController
     @symptoms = Symptom.order(:title)
   end
 
-  def show
-    @symptom = Symptom.find(params[:id])
+  def show; end
+
+  def new
+    @symptom = Symptom.new
+  end
+
+  def create
+    @symptom = Symptom.new(symptom_params)
+    if @symptom.save
+      redirect_to @symptom, notice: '症状を登録しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @symptom.update(symptom_params)
+      redirect_to @symptom, notice: '症状を更新しました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @symptom.destroy
+    redirect_to symptoms_path, notice: '症状を削除しました。'
   end
 
   private
+
+  def set_symptom
+    @symptom = Symptom.find(params[:id])
+  end
 
   def symptom_params
     params.require(:symptom).permit(:title, :summary, :home_care, :checkpoints, :visit_immediate, :visit_hours, :image)
